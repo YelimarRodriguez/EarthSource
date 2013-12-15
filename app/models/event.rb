a67f2215af069351e8d535aa9889ecc50375ed28
Yelimar <rodriguez.yelimar@gmail.com>
2013-12-14 20:42:14 -0500

    Added Associations and Authentication

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+  has_many :rsvps 
+  has_many :users through: :rsvps
+  #Check in the rsvp table for my event id and show me all the users that are 
+  #associated to my event id
+   
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :rsvps
+  has_many :reservations, through: :rsvps, source: :events
+  #source: :event is telling the app to look at the events table but we are calling it
+  #reservations
+  has_many :events 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
