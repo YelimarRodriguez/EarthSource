206e2644c5a46e68664afd95926d30d6f83d746a
Yelimar <rodriguez.yelimar@gmail.com>
2013-12-15 17:30:36 -0500

    Added Panels to events, and created a show page for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,5 @@ class Event < ActiveRecord::Base
   has_many :users, through: :rsvps
   #Check in the rsvp table for my event id and show me all the users that are 
   #associated to my event id
-  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'} 
 
 end
